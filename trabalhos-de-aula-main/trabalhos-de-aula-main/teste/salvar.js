57c3f54b92ff5cfa3fe2465d4eb0c93fa6748fc7
EduardoxxMenezes <162368836+EduardoxxMenezes@users.noreply.github.com>
2024-10-03 00:20:54 -0300

    Add files via upload

---
+++ trabalhos-de-aula-main/trabalhos-de-aula-main/teste/salvar.js
@@ -0,0 +1,17 @@
+let p1 = document.getElementById("p1");
+let p2 = document.getElementById("p2");
+let p3 = document.getElementById("p3");
+let p4 = document.getElementById("p4");
+let Horario = document.getElementById("Horario");
+let data = document.getElementById("Data");
+console.log(Horario)
+// function salvarProduto(){
+//     let novaVenda = {
+//     }
+   
+//     if(p1 == true){
+ 
+
+//     }
+//     return ""
+// }
